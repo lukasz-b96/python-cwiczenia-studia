9421e3346b9bed12737b9f4ea31713fe9bb3a8d0
l00ki-toja <freshbenypl@gmail.com>
2021-10-17 20:52:39 +0200

    fix 2

--- Lekcja2/2.10.py
+++ Lekcja2/2.10.py
@@ -1,8 +1,4 @@
 print("hello world 1")
 print("hello world 1")
-<<<<<<< HEAD
 print("zmiana 3 lini z vscode")
-=======
-print("zmiana 3 lini z gita")
->>>>>>> 63997259fd3c7beded8191c5ccf8b14a27381ca5
 #test pulla
