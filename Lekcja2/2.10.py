55c77ed5bcf937d6e359bad504f652c9f6574e39
l00ki-toja <freshbenypl@gmail.com>
2021-10-18 12:03:32 +0200

    zadania z Lekcji 2

--- Lekcja2/2.10.py
+++ Lekcja2/2.10.py
@@ -1,4 +1,13 @@
-print("hello world 1")
-print("hello world 1")
-print("zmiana 3 lini z vs code")
-#test pulla
+line = 'Lorem ipsum dolor sit amet, ' \
+    'consectetur adipiscing elit.\n' \
+    'Aliquam accumsan sem eu ante facilisis posuere.\n' \
+    'Morbi euismod malesuada diam, ' \
+    'quis porttitor elit venenatis sed.\n' \
+    'Pellentesque suscipit eget erat sed egestas.\n' \
+    'Ut rhoncus massa eget arcu laoreet imperdiet.\n' \
+    'Sed sit amet gravida eros.\n' \
+    'Maecenas auctor mollis mi eget ultrices.\n' \
+    'Quisque viverra pulvinar neque,\t \
+    nec fringilla risus semper eget.'
+    
+print('Liczba wyrazów w line:', len(line.split()))
